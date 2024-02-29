9f71863aa2b1f5a85a3740269e295d7c6c90191d
Noah-Chappell <noahmchappell@gmail.com>
2024-02-28 18:02:30 -0800

    Set up DbInterface object and excecuting DbInterface functions from command line

--- project.py
+++ project.py
@@ -1,5 +1,33 @@
+import sys
+from typing import Callable
+
+class DbInterface:
+    def getFunction(functionName : str) -> Callable | None:
+        '''
+        returns DbInterface function if present, else returns None
+        '''
+        classFunction: callable | None = getattr(DbInterface, functionName, None)
+        if (callable(classFunction)):
+            return classFunction
+        return None
+
+
+def getCommandLineInput() -> list[str]:
+    '''
+    returns command line input without filename
+    '''
+    return sys.argv[1:]
+
+
 def main():
-    print("hello world")
+    # input format:[function arg1 arg2 ...]
+    input: list[str] = getCommandLineInput()
+
+    command: Callable | None = DbInterface.getFunction(input[0])
+    if (command == None):
+        raise ValueError(f'DbInterface has no function {input[0]}')
+    
+    command(*input[1:])
 
 if __name__ == '__main__':
     main()
